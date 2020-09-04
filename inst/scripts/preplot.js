45378ea9b2f618dbebd39d26d9480433ad3f84ae
sqyu <sqyu@uw.edu>
2020-09-04 10:46:52 +0900

    Truncate variable names to 15 chars

--- inst/scripts/preplot.js
+++ inst/scripts/preplot.js
@@ -1,40 +1,48 @@
 var is_X_Y = (typeof vars_X !== 'undefined');
 
-
-
 if (is_X_Y) {
 	for (var varname in window){
-	if (varname.includes("_") && (varname.substring(0,3) == 'cor' || varname.substring(0,4) == 'diff')){ ////
-		window[varname + "_mat"] = [];
-		for (var i = 0; i < window[varname].length; ++i) {
-			for (var j = 0; j < window[varname][i].length; ++j) {
-				window[varname+"_mat"].push({row: j, col: i, value:window[varname][i][j]}); // json written by column in R; so need to reverse when reading in
+		if (varname.includes("_") && (varname.substring(0,3) == 'cor' || varname.substring(0,4) == 'diff')){ ////
+			window[varname + "_mat"] = [];
+			for (var i = 0; i < window[varname].length; ++i) {
+				for (var j = 0; j < window[varname][i].length; ++j) {
+					window[varname+"_mat"].push({row: j, col: i, value:window[varname][i][j]}); // json written by column in R; so need to reverse when reading in
+				}
 			}
+			window[varname] = null;
 		}
-		window[varname] = null;
-	}
 	}
 	var nind_first = dat_first_X.ind.length,
 		nind_second = dat_second_X.ind.length,
 		nvar_X = vars_X.length,
 		nvar_Y = vars_Y.length;
+
+	for (let i = 0; i < nvar_X; i++)
+		vars_X[i] = cut_string(vars_X[i], 15);
+	for (let i = 0; i < nvar_Y; i++)
+		vars_Y[i] = cut_string(vars_Y[i], 15);
+
 } else {
 	for (var varname in window){
-	if (varname.includes("_") && (varname.substring(0,3) == 'cor' || varname.substring(0,4) == 'diff')){ ////
-		window[varname + "_mat"] = [];
-		for (var i = 0; i < window[varname].length; ++i) {
-			for (var j = 0; j <= i; ++j) {
-				window[varname+"_mat"].push({row: j, col: i, value:window[varname][i][j]}); // json written by column in R; so need to reverse when reading in
+		if (varname.includes("_") && (varname.substring(0,3) == 'cor' || varname.substring(0,4) == 'diff')){ ////
+			window[varname + "_mat"] = [];
+			for (var i = 0; i < window[varname].length; ++i) {
+				for (var j = 0; j <= i; ++j) {
+					window[varname+"_mat"].push({row: j, col: i, value:window[varname][i][j]}); // json written by column in R; so need to reverse when reading in
+				}
 			}
+			window[varname] = null;
 		}
-		window[varname] = null;
 	}
-}
+
 	var nind_first = dat_first.ind.length,
 		nind_second = dat_second.ind.length,
 		nvar_X = vars.length,
 		nvar_Y = vars.length,
 		vars_X = vars_Y = vars;
+
+	for (let i = 0; i < nvar_X; i++)
+		vars[i] = cut_string(vars[i], 15);
 }
 
 if (nvar_Y > nvar_X) { // more variables to plot on the x axis, make h smaller and fill the margin
